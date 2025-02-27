e4318c2720c91028cf477e1241d37abe9d277213
TR1432 <ppfaddy@gmail.com>
2025-02-27 07:20:41 +0100

    Feat: Minor changes to petition routes

--- src/routes/petition.js
+++ src/routes/petition.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { getPetitionsController } = require('../controllers/PetitionController');
+const { authenticateUserwebtoken, isCouncil } = require('../middlewares/authenticate');
 const router = express.Router();
 
 router.use(express.json());
 
 router.get("/", getPetitionsController)
 router.get("/:id", getPetitionsController)
-router.post("/", getPetitionsController)
-router.put("/:id", getPetitionsController)
-router.delete("/:id", getPetitionsController)
+router.post("/", authenticateUserwebtoken,  getPetitionsController)
+router.put("/:id", authenticateUserwebtoken, isCouncil, getPetitionsController)
+router.delete("/:id", authenticateUserwebtoken, isAllowed, getPetitionsController)
 
 module.exports = router;
